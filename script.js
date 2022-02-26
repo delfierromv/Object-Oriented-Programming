c5a8308a695794081a2ccee20cbca5f1b16e425a
delfierromv <delfierro.mv@gmail.com>
2022-02-25 19:41:21 -0800

    Coding Challenge

--- script.js
+++ script.js
@@ -256,38 +256,38 @@ sarah.calcAge();
 
 //////////////////////////////////////////////////
 //CODING CHALLENGE #2
-
-const CarCl = class{
-  constructor(make, speed){
+class Car {
+  constructor(make,speed){
     this.make = make;
     this.speed = speed;
   }
-  
+
   accelerate = function(){
-  this.speed += 10;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
-
-brake = function(){
-  this.speed -= 5;
-  console.log(`${this.make} is going at ${this.speed} km/h`);
-};
-
-
-  get speedUS(){
-    return `${this.speed/1.6} mi/h`;
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
   }
 
-  set speedUS(speed){
-    this.speed = speed *1.6;
+  brake = function(){
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
   }
-};
 
-const ford = new CarCl('Ford', 120)
-console.log(ford.speedUS)
+  get speedUs(){
+    return this.speed/1.6;
+  }
+  set speedUs(speed){
+    this.speed = speed*1.6;
+  }
+}
+
+const ford = new Car('Ford', 120);
+console.log(ford);
+ford.accelerate();
+ford.accelerate();
 ford.accelerate();
 ford.accelerate();
 ford.brake();
 
-ford.speedUS = 50;
-console.log(ford);
+console.log(ford.speed);
+
+

d573d99423f501a4f64c8f887f76a6bc5c331cdd
delfierromv <delfierro.mv@gmail.com>
2022-03-03 15:50:05 -0800

    Bank account Example
    Another example of ES6 classes using bank account from Bankist App. Example of public interface of objects including the deposit, withdraw approveLoan, request loan methods.. which demonstrates the necessity of data encapsulation and data privacy.

--- script.js
+++ script.js
@@ -436,7 +436,7 @@ const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
 console.log(martha);
 martha.introduce()
 martha.calcAge();
-*/
+
 
 //////////////////////////////////////////////
 //INHERITANCE BETWEEN "CLASSES": OBJECT.CREATE
@@ -468,5 +468,52 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
 
+/////////////////////////////////
+//ANOTHER EXAMPLE
+
+class Account{
+  constructor(owner, currency, pin){
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+//PUBLIC INTERFACE OF OUR OBJECTS
+  deposit(val){
+    this.movements.push(val)
+  }
+
+  withdraw(val){
+    this.deposit(-val)
+  }
+
+  approveLoan(val){
+    return true;
+  }
+  requestLoan(val){
+    if(this.approveLoan(val)){
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111,);
+console.log(acc1);
+
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140)
+acc1.requestLoan(1000)
+//this approve loan method should be an internal method that a regular user should not be able to access-- this is why we need data encapsulation and data privacy.
+acc1.approveLoan(1000);
+console.log(acc1);
+console.log(acc1.pin);
 
